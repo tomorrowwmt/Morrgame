f38af220fbddce574d5308a4304e0a08998329ad
tomorrowwmt <1312030474@qq.com>
2020-05-19 23:26:10 +0800

    一阶段整改

--- src/main/java/com/springwork/netty/handler/HelloClientHandler.java
+++ src/main/java/com/springwork/netty/handler/HelloClientHandler.java
@@ -1,24 +1,20 @@
 package com.springwork.netty.handler;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
 @Slf4j
-public class HelloClientHandler extends SimpleChannelInboundHandler<String> {
+public class HelloClientHandler extends ChannelInboundHandlerAdapter {
     public static String Name = "";
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+    //@Override
+    public  void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("Server say : " + msg);
-        if (null == msg) {
-            return;
-        }
-        //收到数
-        String data= msg;
-        if("Morr游戏客户端登陆成功".equals(data)){
+        System.out.println(777777);
+        //收到数据
+        String data= (String) msg;
+        if("nice".equals(data)){
             System.out.println("牛牛客户端启动成功！");
-            System.out.println("请选择序号:1.登录   2.注册");
+            System.out.println("请选择序号:1.登录2.注册");
             String cli = read();
             if("1".equals(cli)){
                 System.out.println("请输入用户名：");
@@ -35,7 +31,7 @@ public class HelloClientHandler extends SimpleChannelInboundHandler<String> {
             }
         }else if(data.contains("wbl2")||data.contains("wbl3")){
             System.out.println(data.substring(4));
-            System.out.println("请选择序号:1.登录   2.注册");
+            System.out.println("请选择序号:1.登录 2.注册");
             while(true){
                 String cli = read();
                 if("1".equals(cli)){
@@ -52,45 +48,28 @@ public class HelloClientHandler extends SimpleChannelInboundHandler<String> {
                     System.out.println("指令输入有误，请重新输入:1.登录   2.注册");
                 }
             }
-        }else if(data.contains("wbl1")||data.contains("wbl4")){
-            System.out.println(data.substring(4));
-            System.out.println("游戏开始！请输入命令序号：1打印 2.移动  3.退出");
+        }else if(data.contains("wbl1")){
+            //System.out.println(data.substring(4));
+            System.out.println("游戏开始！请输入命令序号：aor,move,exit\n");
             while(true){
                 String cli = read();
-                if("1".equals(cli)){
-                    ctx.channel().writeAndFlush("wbl3"+Name);
+                if("aor".equals(cli)){
+                    ctx.channel().writeAndFlush("收到打印消息请稍后----");
+                    System.out.println("怪兽");
                     break;
-                }else if("2".equals(cli)){
-                    ctx.channel().writeAndFlush("wbl4"+Name);
+                }else if("move".equals(cli)){
+                    ctx.channel().writeAndFlush("收到移动消息请稍后----");
+                    System.out.println("兽");
                     break;
-                }else if("3".equals(cli)){
-                    ctx.channel().writeAndFlush("wbl5"+Name);
+                }else if("exit".equals(cli)){
+                    ctx.channel().writeAndFlush("收到退出消息请稍后");
                     System.out.println("客户端正常登出，"+Name+"账号非常安全！");
                     System.exit(1);
                 }else{
-                    System.out.println("指令输入有误，请重新输入:打印 2.移动  3.退出");
+                    System.out.println("指令输入有误，请输入命令序号：aor,move,exit");
                 }
             }
             //开始返回结果
-        }else if(data.contains("wbl5") || data.contains("wbl6")){
-            System.out.println(data.substring(4));
-            while(true){
-                System.out.println("\n\n游戏开始！请输入命令序号：1打印 2.移动  3.退出");
-                String cli = read();
-                if("1".equals(cli)){
-                    ctx.channel().writeAndFlush("wbl3"+Name);
-                    break;
-                }else if("2".equals(cli)){
-                    ctx.channel().writeAndFlush("wbl4"+Name);
-                    break;
-                }else if("3".equals(cli)){
-                    System.out.println("客户端正常登出，"+Name+"账号非常安全！");
-                    ctx.channel().writeAndFlush("wbl5"+Name);
-                    System.exit(1);
-                }else{
-                    System.out.println("指令输入有误，请重新输入:1打印 2.移动  3.退出");
-                }
-            }
         }
     }
 
@@ -115,7 +94,7 @@ public class HelloClientHandler extends SimpleChannelInboundHandler<String> {
         return str;
     }
     public static void sendMsg(Channel channel) {
-        String str = "login";
+        String str = "logon";
         channel.writeAndFlush(str);
     }
 }
--- src/main/java/com/springwork/netty/handler/HelloServerHandler.java
+++ src/main/java/com/springwork/netty/handler/HelloServerHandler.java
@@ -7,6 +7,7 @@ import io.netty.channel.Channel;
 
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
@@ -24,7 +25,7 @@ import org.springframework.stereotype.Service;
 //地址是import io.netty.channel.ChannelHandler.Sharable;
 
 //继承Netty提供的通道传入处理器类，只要复写方法就可以了，简化开发
-public class HelloServerHandler extends SimpleChannelInboundHandler<String> {
+public class HelloServerHandler extends ChannelInboundHandlerAdapter {
     public static Map<String,String> user = new HashMap<String, String>();
     //获取现有通道，一个通道channel就是一个socket链接在这里
     public static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
@@ -51,34 +52,22 @@ public class HelloServerHandler extends SimpleChannelInboundHandler<String> {
 
     //消息读取有两个方法，channelRead和channelRead0，其中channelRead0可以读取泛型，常用
     //收到消息打印出来，并返还客户端消息
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         // 收到消息直接打印输出
         System.out.println(ctx.channel().remoteAddress() + " Say : " + msg);
-        String data=msg;
-        if("login".equals( data)){
-            ctx.channel().writeAndFlush("客户端登陆成功");
+        String data= (String) msg;
+        if("logon".equals(data)){
+            ctx.channel().writeAndFlush("nice");
             return;
-        }else if( data.contains("wb1")) {
-            String name =  data.substring(4);
-            if (user.containsKey(name)) {
-                user.put(name, "sucess");
-                ctx.channel().writeAndFlush("wbl1牛气冲天的用户-" + name + "-登录成功！");
-                return;
-            } else {
-                ctx.channel().writeAndFlush("wbl2" + data.substring(4) + "-用户未注册！");
-                return;
-            }
-        }else if( data.contains("wbl2")){//注册
-            String name =  data.substring(4);
-            if(user.containsKey(name)){
-                ctx.channel().writeAndFlush("wbl3"+ data.substring(4) +"-用户已注册，请换个其他名字！");
-                return;
-            }else{
-                user.put( data.substring(4), "1_20");
-                ctx.channel().writeAndFlush("wbl4牛气冲天的用户-"+ data.substring(4) +"-注册并登录成功！");
-                return;
-            }
+        }else if(data.contains("wbl1")) {
+            String name =data.substring(4);
+            user.put(name, "sucess");
+            ctx.channel().writeAndFlush("wbl1牛气冲天的用户-" + name + "-登录成功！");
+            return;
+        }else if(data.contains("wbl2")) {
+            ctx.channel().writeAndFlush("wbl2" +"-用户未注册！");
+            return ;
         }
         // 返回客户端消息 - 我已经接收到了你的消息
         ctx.writeAndFlush("------Received your message !\n");
