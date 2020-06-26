048f1c98c3048ac0c342c341394468be8f53a104
tomorrowwmt <1312030474@qq.com>
2020-06-26 10:24:11 +0800

    撰写SpringUtil类获取bean，服务器启动自动加载ApplictaionConext

---
+++ src/main/java/cn/pomit/springwork/netty/UtilSpring/SpringUtil.java
@@ -0,0 +1,27 @@
+package cn.pomit.springwork.netty.UtilSpring;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class SpringUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext; // Spring应用上下文环境
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) throws BeansException {
+        return (T) applicationContext.getBean(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<?> clz) throws BeansException {
+        return (T) applicationContext.getBean(clz);
+    }
+}
